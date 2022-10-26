3ddba3dae801e26a87c62d49422dcdcc8b80b0a0
utun <ubun@email.com>
2022-10-27 00:05:55 +0900

    s

--- servletJSPII/src/main/java/com/fileupload/FileUploadServlet.java
+++ servletJSPII/src/main/java/com/fileupload/FileUploadServlet.java
@@ -1,4 +1,4 @@
-package fileupload;
+package com.fileupload;
 
 import java.io.File;
 import java.io.IOException;
--- servletJSPII/src/main/java/com/fileupload/UploadServlet.java
+++ servletJSPII/src/main/java/com/fileupload/UploadServlet.java
@@ -1,4 +1,4 @@
-package fileupload;
+package com.fileupload;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
