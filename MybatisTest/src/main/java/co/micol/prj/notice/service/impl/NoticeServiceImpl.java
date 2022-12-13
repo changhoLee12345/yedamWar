0fc48abb24fd620e51e1e4537d15c970ca71030c
ubuntu <ubuntu@email.com>
2022-12-13 23:02:57 +0900

    f

--- MybatisTest/src/main/java/co/micol/prj/notice/service/impl/NoticeServiceImpl.java
+++ MybatisTest/src/main/java/co/micol/prj/notice/service/impl/NoticeServiceImpl.java
@@ -2,34 +2,41 @@ package co.micol.prj.notice.service.impl;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+
+import co.micol.prj.common.DataSource;
+import co.micol.prj.notice.mapper.NoticeMapper;
 import co.micol.prj.notice.service.NoticeService;
 import co.micol.prj.notice.vo.NoticeVO;
 
 public class NoticeServiceImpl implements NoticeService {
 
+	SqlSession sqlSession = DataSource.getInstance().openSession(true);
+	NoticeMapper mapper = sqlSession.getMapper(NoticeMapper.class);
+
 	@Override
 	public List<NoticeVO> noticeList() {
-		return null;
+		return mapper.noticeList();
 	}
 
 	@Override
 	public NoticeVO noticeSelect(NoticeVO vo) {
-		return null;
+		return mapper.noticeSelect(vo);
 	}
 
 	@Override
 	public int noticeInsert(NoticeVO vo) {
-		return 0;
+		return mapper.noticeInsert(vo);
 	}
 
 	@Override
 	public int noticeUpdate(NoticeVO vo) {
-		return 0;
+		return mapper.noticeUpdate(vo);
 	}
 
 	@Override
 	public int noticeDelete(NoticeVO vo) {
-		return 0;
+		return mapper.noticeDelete(vo);
 	}
 
 }
