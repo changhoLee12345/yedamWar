ca4cec10219e855f96397cd93556d37ed8234bb0
ubuntu <ubuntu@email.com>
2022-12-28 14:54:15 +0900

    d

---
+++ HelloApp/src/main/resources/config/table.sql
@@ -0,0 +1,7 @@
+create table tbl_book(
+ book_code varchar2(10) primary key,
+ book_title varchar2(100) not null,
+ book_title varchar2(100) not null,
+ book_press varchar2(100) not null,
+ book_price number default 0
+);
