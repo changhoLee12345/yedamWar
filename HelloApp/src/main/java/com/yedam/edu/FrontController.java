d77c3e91ce2d7965b7af0e5554c6f78c4f811e25
'leadon' <'leadon@pc.com'>
2023-03-22 00:15:02 +0900

    x

--- HelloApp/src/main/java/com/yedam/edu/FrontController.java
+++ HelloApp/src/main/java/com/yedam/edu/FrontController.java
@@ -78,8 +78,8 @@ public class FrontController extends HttpServlet {
 		map.put("/addBookJson.do", new BookAddJson());
 
 		// 공지사항관련.
-		map.put("/noticeList.do", new NoticeList()); //공지사항목록.
-		map.put("/noticeListJsonForm.do", new NoticeListJsonForm()); //공지사항등록폼(json)
+		map.put("/noticeList.do", new NoticeList()); // 공지사항목록.
+		map.put("/noticeListJsonForm.do", new NoticeListJsonForm()); // 공지사항등록폼(json)
 		map.put("/noticeListJson.do", new NoticeListJson());
 		map.put("/deleteNoticeJson.do", new DeleteNoticeJson());
 		map.put("/getNotice.do", new NoticeSearch());
@@ -122,41 +122,43 @@ public class FrontController extends HttpServlet {
 	// 요청을 분석하고 실행, 결과를 돌려주는 곳
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
 		request.setCharacterEncoding("utf-8"); // 한글깨짐방지
 		String uri = request.getRequestURI(); // 요청한 uri를 구함
 		String contextPath = request.getContextPath(); // 루트를 구함,context path
 		String page = uri.substring(contextPath.length()); // 실제 수행할 요청을 구함
 		System.out.println("====> " + page);
 
+		String viewPage = "main.do";
+		RequestDispatcher dispatcher = null;
+
 		Command command = map.get(page); // init 메소드에서 수행할 명령을 가져온다.
 
-		String viewPage = command.exec(request, response); // 명령을 수행하고 결과를 돌려받음
+		if (command != null) {
+			viewPage = command.exec(request, response); // 명령을 수행하고 결과를 돌려받음
 
-		// viewResolve 파트
-		if (viewPage != null && !viewPage.endsWith(".do")) {
-			// ajax 처리
-			if (viewPage.startsWith("ajax:")) {
-				response.setContentType("text/html; charset=UTF-8");
-				response.getWriter().append(viewPage.substring(5));
+			// viewResolve 파트
+			if (viewPage != null && !viewPage.endsWith(".do")) {
+
+				if (viewPage.startsWith("ajax:")) {
+					// ajax 처리
+					response.setContentType("text/html; charset=UTF-8");
+					response.getWriter().append(viewPage.substring(5));
+					return;
+
+				} else if (viewPage.endsWith(".jsp")) {
+					// jsp 처리.
+					viewPage = "/WEB-INF/views/" + viewPage; // 타일즈를 안태움
+				}
+
+				// tiles처리하는 곳.
+				dispatcher = request.getRequestDispatcher(viewPage);
+				dispatcher.forward(request, response);
 				return;
 			}
-
-			// 타일즈 돌아가는곳
-			if (!viewPage.endsWith(".tiles")) {
-				viewPage = "/WEB-INF/views/" + viewPage + ".jsp"; // 타일즈를 안태움
-			}
-
-			// tiles처리하는 곳.
-			RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
-			dispatcher.forward(request, response);
-
-		} else {
-
-			if (viewPage == null) {
-				viewPage = "main.do";
-			}
-			response.sendRedirect(viewPage); // *.do 로 들어올때 돌아가는 곳
-
 		}
+
+		response.sendRedirect(viewPage); // *.do 로 들어올때 돌아가는 곳
+
 	}
 }
