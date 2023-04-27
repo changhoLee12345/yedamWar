1babf27a648771f9882d22df49bb56ba5eef86a8
user <user@DESKTOP-AJJR5DM>
2023-04-27 17:56:10 +0900

    ffff

---
+++ MyProject/src/main/java/com/yedam/prod/mapper/ProdMapper.java
@@ -0,0 +1,11 @@
+package com.yedam.prod.mapper;
+
+import java.util.List;
+
+import com.yedam.prod.domain.ProductVO;
+
+public interface ProdMapper {
+	public List<ProductVO> prodList();
+	public ProductVO selectProduct(String prodCode);
+	public List<ProductVO> relatedProduct();
+}
---
+++ MyProject/src/main/java/com/yedam/prod/service/ProdServiceImpl.java
@@ -0,0 +1,30 @@
+package com.yedam.prod.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.yedam.common.DataSource;
+import com.yedam.prod.domain.ProductVO;
+import com.yedam.prod.mapper.ProdMapper;
+
+public class ProdServiceImpl implements ProdService {
+	SqlSession session = DataSource.getInstance().openSession();
+	ProdMapper mapper = session.getMapper(ProdMapper.class);
+
+	@Override
+	public List<ProductVO> prodList() {
+		return mapper.prodList();
+	}
+
+	@Override
+	public ProductVO getProdeuct(String prodCode) {
+		return mapper.selectProduct(prodCode);
+	}
+
+	@Override
+	public List<ProductVO> relatedProduct() {
+		return mapper.relatedProduct();
+	}
+
+}
