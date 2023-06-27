c2c9f1baa45778d2dab3d0a77fd234a4ee779d08
winnie <winnie@DESKTOP-F7LQ917.mshome.net>
2023-06-28 08:54:19 +0900

    ff

--- HelloApp/src/resources/java/table.sql
+++
@@ -1,7 +0,0 @@
-create table tbl_book(
- book_code varchar2(10) primary key,
- book_title varchar2(100) not null,
- book_title varchar2(100) not null,
- book_press varchar2(100) not null,
- book_price number default 0
-);
